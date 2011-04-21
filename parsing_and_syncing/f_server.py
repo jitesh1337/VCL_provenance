ce9f22307fcb54a2ee309cf5cc3974fd7d9a8aac
Salil Kanitkar <slashforu@gmail.com>
2011-04-20 22:47:44 -0400

    Parse the output of parse_sar script
    Put it in a new table. Have a client-server wrapper around it.
    
    Signed-off-by: Salil Kanitkar <slashforu@gmail.com>

--- parsing_and_syncing/f_server.py
+++ parsing_and_syncing/f_server.py
@@ -2,10 +2,28 @@
 
 import socket
 import sys
+import math
+import os
+
+if len(sys.argv) != 2:
+	sys.exit("Provide the mn_id as command line argument!")
+
+mn_id = int(sys.argv[1])
+
+if mn_id == 1:
+	addr = "192.168.40.1"
+if mn_id == 2:
+	addr = "192.168.5.1"
+if mn_id == 3:
+	addr = "192.168.60.1"
+
+oldout = sys.stdout
+fsock = open('/tmp/dyn_logfile', 'w')
+sys.stdout = fsock
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-serversocket.bind(('192.168.1.114', 5555))
+serversocket.bind((addr, 5000))
 
 serversocket.listen(1)
 
@@ -16,6 +34,14 @@ while 1:
 	line = clientfile.readline()
 	if line == '':
 		break
-	print "%s" % (line)
+	line_1 = line.split("\n")
+	print "%s" % (line_1[0])
 clientsocket.close()
+
+sys.stdout = oldout
+fsock.close()
+
+set_mn = sys.argv[1]
+str = "python /root/VCL_provenance/parsing_and_syncing/dyn_parse.py " + set_mn + " /tmp/dyn_logfile"
+os.system(str)
 
