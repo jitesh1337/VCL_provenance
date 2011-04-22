6542ab6ba42f9f693fe16eee799802a09760b72f
Jitesh Shah <jitesh.1337@gmail.com>
2011-04-22 18:20:45 -0400

    Final arch setup on images
    Signed-off-by: Jitesh Shah <jitesh.1337@gmail.com>

---
+++ actual_provenance/images/image_to_mgmt_client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import socket
+import sys
+
+#clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#clientsocket.connect(('192.168.50.1', 5555))
+while 1:
+        fp = open("parse_sar_output","r")
+        while 1:
+                line = fp.readline()
+                if line.strip() == "END":
+                        break;
+                if line == '':
+                        break;
+                print line,
+                #clientsocket.send(line)
+#       clientsocket.close()
+#       clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#       clientsocket.connect(('192.168.50.1', 5555))
+        fp.close()
+                #clientsocket.send(line)
+
+#clientsocket.close()
