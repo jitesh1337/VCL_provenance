4872be8c637f5c9098c5b0a622ed0483a5406184
Salil Kanitkar <slashforu@gmail.com>
2011-04-23 18:54:14 -0400

    Post-processing of the dynamic stuff.
    Signed-off-by: Salil Kanitkar <slashforu@gmail.com>

--- parsing_and_syncing/dyn_parse.py
+++ parsing_and_syncing/dyn_parse.py
@@ -25,8 +25,12 @@ def check_if_exists(image_id, log_id):
 	f_cursor.execute(sql)
 
 	if f_cursor.rowcount == 0:
+		f_cursor.close()
+		f_conn.close()
 		return 1
 	else:
+		f_cursor.close()
+		f_conn.close()
 		return 0
 
 def get_row_count(image_id, log_id):
@@ -43,6 +47,8 @@ def get_row_count(image_id, log_id):
 	rows = g_cursor.fetchone();
 	count = rows[17]
 
+	g_cursor.close()
+	g_conn.close()
 	print "count is : %s" % (count)
 	return count
 
@@ -60,8 +66,114 @@ def get_cpu_idle(image_id, log_id):
 	row = g_cursor.fetchone()
 	c_idle = int(row[5])
 
+	g_cursor.close()
+	g_conn.close()
 	print "cpu_idle is : %d" % (c_idle)
 	return c_idle
+
+def get_cpu_peak(image_id, log_id):
+
+	g_conn	= MySQLdb.connect ( host = "localhost",
+					user = "root",
+					passwd = "sm3",
+					db = "provenance")
+	g_cursor = g_conn.cursor()
+
+	sql = "select  * from mn_dyn_info where mn_id = %d and image_id = %d and log_id = %d" % (set_mn,image_id,log_id)
+	g_cursor.execute(sql)
+
+	row = g_cursor.fetchone()
+	c_peak = int(row[6])
+
+	g_cursor.close()
+	g_conn.close()
+	print "cpu_peak is : %d" % (c_peak)
+	return c_peak
+
+def get_mem_size(image_id, log_id):
+
+	g_conn	= MySQLdb.connect ( host = "localhost",
+					user = "root",
+					passwd = "sm3",
+					db = "provenance")
+	g_cursor = g_conn.cursor()
+
+	sql = "select  * from mn_dyn_info where mn_id = %d and image_id = %d and log_id = %d" % (set_mn,image_id,log_id)
+	g_cursor.execute(sql)
+
+	row = g_cursor.fetchone()
+	m_size = int(row[8])
+
+	print "mem_size is : %d" % (m_size)
+	return m_size
+
+def get_mem_free(image_id, log_id):
+
+	g_conn	= MySQLdb.connect ( host = "localhost",
+					user = "root",
+					passwd = "sm3",
+					db = "provenance")
+	g_cursor = g_conn.cursor()
+
+	sql = "select  * from mn_dyn_info where mn_id = %d and image_id = %d and log_id = %d" % (set_mn,image_id,log_id)
+	g_cursor.execute(sql)
+
+	row = g_cursor.fetchone()
+	m_free = int(row[9])
+
+	print "mem_free is : %d" % (m_free)
+	return m_free
+
+def get_mem_used(image_id, log_id):
+
+	g_conn	= MySQLdb.connect ( host = "localhost",
+					user = "root",
+					passwd = "sm3",
+					db = "provenance")
+	g_cursor = g_conn.cursor()
+
+	sql = "select  * from mn_dyn_info where mn_id = %d and image_id = %d and log_id = %d" % (set_mn,image_id,log_id)
+	g_cursor.execute(sql)
+
+	row = g_cursor.fetchone()
+	m_used = int(row[10])
+
+	print "mem_used is : %d" % (m_used)
+	return m_used
+
+def get_io_block_reads(image_id, log_id):
+
+	g_conn	= MySQLdb.connect ( host = "localhost",
+					user = "root",
+					passwd = "sm3",
+					db = "provenance")
+	g_cursor = g_conn.cursor()
+
+	sql = "select  * from mn_dyn_info where mn_id = %d and image_id = %d and log_id = %d" % (set_mn,image_id,log_id)
+	g_cursor.execute(sql)
+
+	row = g_cursor.fetchone()
+	io_b_reads = int(row[12])
+
+	print "io_b_reads is : %d" % (io_b_reads)
+	return io_b_reads
+
+def get_io_block_writes(image_id, log_id):
+
+	g_conn	= MySQLdb.connect ( host = "localhost",
+					user = "root",
+					passwd = "sm3",
+					db = "provenance")
+	g_cursor = g_conn.cursor()
+
+	sql = "select  * from mn_dyn_info where mn_id = %d and image_id = %d and log_id = %d" % (set_mn,image_id,log_id)
+	g_cursor.execute(sql)
+
+	row = g_cursor.fetchone()
+	io_b_writes = int(row[13])
+
+	print "io_b_reads is : %d" % (io_b_writes)
+	return io_b_writes
 
 
 #Connection to the required Database (Provenance Image)
@@ -179,8 +291,29 @@ while True:
 		cursor = conn.cursor()
 
 		count = get_row_count(IMAGE_ID, LOG_ID)
+
 		c_idle = get_cpu_idle(IMAGE_ID, LOG_ID)
 		CPU_IDLE = ( float(CPU_IDLE) + (count*c_idle) ) / (count+1)
+
+		c_peak = get_cpu_peak(IMAGE_ID, LOG_ID)
+		if CPU_PEAK < c_peak:
+			CPU_PEAK = c_peak
+
+		m_size = get_mem_size(IMAGE_ID, LOG_ID)
+		MEM_SIZE = ( float(MEM_SIZE) + (count*m_size) ) / (count+1)
+
+		m_free = get_mem_free(IMAGE_ID, LOG_ID)
+		MEM_FREE = ( float(MEM_FREE) + (count*m_free) ) / (count+1)
+
+		m_used = get_mem_used(IMAGE_ID, LOG_ID)
+		MEM_USED = ( float(MEM_USED) + (count*m_used) ) / (count+1)
+
+		io_b_reads = get_io_block_reads(IMAGE_ID, LOG_ID)
+		IO_BLOCK_READS = ( float(IO_BLOCK_READS) + io_b_reads )
+
+		io_b_writes = get_io_block_writes(IMAGE_ID, LOG_ID)
+		IO_BLOCK_WRITES = ( float(IO_BLOCK_WRITES) + io_b_writes )
+
 		count = count + 1;
 		sql = "update mn_dyn_info set cpu_num_cores=%s, cpu_idle=%s, cpu_peak=%s, cpu_loadavg=%s, mem_size=%s, mem_free=%s, mem_used=%s, mem_peak_used=%s, io_block_reads=%s, io_block_writes=%s, eth0_rx=%s, eth0_tx=%s, eth1_rx=%s, eth1_tx=%s, row_count=%d where mn_id=%d and image_id=%d and log_id=%d " % (CPU_NUM_CORES,CPU_IDLE,CPU_PEAK,CPU_LOADAVG,MEM_SIZE,MEM_FREE,MEM_USED,MEM_PEAK_USED, IO_BLOCK_READS,IO_BLOCK_WRITES,eth0_rx,eth0_tx,eth1_rx,eth1_tx, count, set_mn, IMAGE_ID, RESERVATION_ID)	
 		print sql
