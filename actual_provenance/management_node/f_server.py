5f4ff87b8beda8c27717f975910ef783d8edc6ce
Jitesh Shah <jitesh.1337@gmail.com>
2011-04-24 16:39:24 -0400

    Final changes to f_server.py
    Signed-off-by: Jitesh Shah <jitesh.1337@gmail.com>

--- actual_provenance/management_node/f_server.py
+++ actual_provenance/management_node/f_server.py
@@ -15,17 +15,23 @@ serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('192.168.50.1', 5555))
 serversocket.listen(100)
 
-#provsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#provsocket.connect(('127.0.0.1', 5555))
+prov_addr=commands.getoutput("ifconfig gre1 | grep \"inet addr\" | awk '{print $2}' | cut -d ':' -f 2")
+prov_addr=prov_addr.replace(".2", ".1");
 
 while True:
         (clientsocket, addr) = serversocket.accept()
         clientfile = clientsocket.makefile('r', 0)
 
+	provsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	provsocket.connect((prov_addr, 5000))
+
 	if clientsocket.getpeername()[0] == '192.168.50.1':
 		print "IMAGE_ID=" + "0"
+		provsocket.send("IMAGE_ID=" + "0\n")
 		print "LOG_ID=" + "0"
+		provsocket.send("LOG_ID=" + "0\n")
 		print "RESERVATION_ID=" + "0"
+		provsocket.send("RESERVATION_ID=" + "0\n")
 	else:
 		#Start a connection
 		cursor = conn.cursor()
@@ -34,15 +40,18 @@ while True:
 		cursor.execute(qstr);
         	row = cursor.fetchone();
 		print "IMAGE_ID=" + str(row[0])
+		provsocket.send("IMAGE_ID=" + str(row[0]) + "\n")
 		print "LOG_ID=" + str(row[1])
+		provsocket.send("LOG_ID=" + str(row[1]) + "\n")
 		print "RESERVATION_ID=" + str(row[2])
+		provsocket.send("RESERVATION_ID=" + str(row[2]) + "\n")
 		cursor.close()
 
         for line in clientfile:
                 print "%s" % (line),
-                #provsocket.send(line)
+                provsocket.send("%s" % (line))
 
+	provsocket.close();
         clientsocket.close()
 
 conn.close()
-#provsocket.close();
